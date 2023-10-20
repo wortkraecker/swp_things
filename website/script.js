3b2c66af6d4ee2f957c08874a038e566d715e4d9
wortkraecker <72093685+wortkraecker@users.noreply.github.com>
2023-10-20 09:57:09 +0200

    Add files via upload

---
+++ website/script.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const maxVerstappenElement = document.querySelector(".max-verstappen");
+
+    maxVerstappenElement.addEventListener("mouseover", function () {
+        maxVerstappenElement.style.color = "blue";
+    });
+
+    maxVerstappenElement.addEventListener("mouseout", function () {
+        maxVerstappenElement.style.color = "";
+    });
+
+    maxVerstappenElement.addEventListener("click", function () {
+        alert("GOAT btw");
+    });
+});
